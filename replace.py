d9279987b731228c678579e112dfe495e38595d4
Roman Kapuścik <70838513+RomanKapuscik@users.noreply.github.com>
2024-01-31 17:49:50 +0100

    Dodano obsługę błędów

--- replace.py
+++ replace.py
@@ -3,15 +3,28 @@ import sys
 
 def search_and_replace(path: str = '', search: str = '', replace: str = ''):
     """Replace searched word in the file from the path."""
-    with open(path, 'r') as file:
-        file_contents = file.read()
-
-        updated_contents = file_contents.replace(search, replace)
+    try:
+        with open(path, 'r') as file:
+            file_contents = file.read()
+            updated_contents = file_contents.replace(search, replace)
+    except FileNotFoundError:
+        print(f'File "{path}" not found')
+        exit()
 
     with open(path, 'w') as file:
         file.write(updated_contents)
 
+    with open(path, 'r') as file:
+        file_contents = file.read()
+    if search in file_contents:
+        print('Something went wrong')
+    else:
+        print('All done')
 
-# To call the function with parameters from cmd add: file path, search world and replace world
-# example: py -3 replace.py plik.txt ServerName=Server2 ServerName=Server77
-search_and_replace(sys.argv[1], sys.argv[2], sys.argv[3])
+
+try:
+    search_and_replace(sys.argv[1], sys.argv[2], sys.argv[3])
+except IndexError:
+    print("""To call the function with parameters from cmd add: file path, search world and replace world
+example: py -3 replace.py plik.txt old new""")
+    exit()
