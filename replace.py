35173cda310220e7a288c2b9e1bbeb0e86b58bbb
roman <roman.kapuscik@outlook.com>
2024-03-09 19:26:35 +0100

    creating backup functionality added

--- replace.py
+++ replace.py
@@ -1,8 +1,52 @@
 import sys
 
+import os.path
+from datetime import datetime
+
+path_to_file: str
+searched_world: str
+world_replace: str
+
+try:
+    path_to_file = sys.argv[1]
+    searched_world = sys.argv[2]
+    world_replace = sys.argv[3]
+except IndexError:
+    print("""To call the function with parameters from cmd add: file path, search world and replace world
+example: py -3 replace.py plik.txt old new""")
+    exit()
+
+
+def create_backup_folder():
+    folder_name = path_to_file.split('.')[0] + '_' + datetime.now().strftime('%Y%m%d')
+    if not os.path.isdir(f'C:\\Backups\\{folder_name}'):
+        os.makedirs(f'C:\\Backups\\{folder_name}')
+    return f'C:\\Backups\\{folder_name}'
+
+
+def create_backup_file(path: str):
+    directory = create_backup_folder()
+    backup_file_name = path.split('.')[0] + '_' + datetime.now().strftime('%Y%m%d%H%M')
+    full_path = f'{directory}/{backup_file_name}'
+    try:
+        with open(path, 'r') as file:
+            file_contents = file.read()
+    except FileNotFoundError:
+        print(f'File "{path}" not found')
+        exit()
+
+    with open(full_path, 'w') as file:
+        file.write(file_contents)
+
 
 def search_and_replace(path: str = '', search: str = '', replace: str = ''):
-    """Replace searched word in the file from the path."""
+    """Replace searched word in the file from the path.
+    :param path: path to the file to replace
+    :param search: word to be replaced
+    :param replace: word to be used to replace the searched word"""
+
+    create_backup_file(path)
+
     try:
         with open(path, 'r') as file:
             file_contents = file.read()
@@ -23,8 +67,8 @@ def search_and_replace(path: str = '', search: str = '', replace: str = ''):
 
 
 try:
-    search_and_replace(sys.argv[1], sys.argv[2], sys.argv[3])
-except IndexError:
+    search_and_replace(path=path_to_file, search=searched_world, replace=world_replace)
+except AttributeError:
     print("""To call the function with parameters from cmd add: file path, search world and replace world
 example: py -3 replace.py plik.txt old new""")
     exit()
