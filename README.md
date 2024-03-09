c37068db88b80b753e028dd0146dcfa2307be3c0
roman <roman.kapuscik@outlook.com>
2024-03-09 20:41:15 +0100

    creating retention for baclups and update in readme

--- README.md
+++ README.md
@@ -3,8 +3,15 @@ Skrypt na zalliczenie
 
 W ramach zaliczenia prosty skrypt do podmiany tekstu w plikach tekstowych. Np zamiana nazwy serwera w plikach konfiguracyjnych, jeśli mamy dokonać takich zmian na 100 urządzeniach.
 
-Można podać parametry bezpośrednio z konsoli w kolejności: ścieżka, słowo do zamiany, na jakie słowo wymieniamy.
+Można podać parametry bezpośrednio z konsoli w kolejności: ścieżka, słowo do zamiany i na jakie słowo wymieniamy.
 
 np. py -3 plik.txt stare nowe
+
+Skrypt:
+- w pierwszej kolejnosci sprawdzi czy istnieje już katalog z backupami dla danego pliku, jesli nie to taki stworzy.
+- Następnie stworzy backup pliku przed zmianą z aktualną datą i godziną.
+- Sprawdzamy ile mamy backupów danego pliku, jesli więcej niż 5 to usuwamy starszse.
+- Na końcu wykona podmianę w pliku docelowym
+
 
 Pozdrawiam
--- replace.py
+++ replace.py
@@ -1,8 +1,8 @@
 """
 Created on Sat April 09.03.2024
 """
-import sys
 import os.path
+import sys
 from datetime import datetime
 
 path_to_file: str
@@ -19,19 +19,30 @@ except IndexError:
     sys.exit()
 
 
-def create_backup_folder():
-    """Create the backup folder for files to be changed."""
+def create_backup_folder() -> str:
+    """Create the backup folder for files to be changed.
+    :return: the backup folder path"""
     folder_name = path_to_file.split('.')[0] + '_' + datetime.now().strftime('%Y%m%d')
     if not os.path.isdir(f'C:\\Backups\\{folder_name}'):
         os.makedirs(f'C:\\Backups\\{folder_name}')
-    return f'C:\\Backups\\{folder_name}'
+    return f'C:\\Backups\\{folder_name}\\'
 
 
-def create_backup_file(path: str):
+def check_how_many_files(directory: str):
+    """Check how backups is already created, and if there are more than five remove the oldest one."""
+    file_list = os.listdir(directory)
+    file_list.sort()
+    if len(file_list) > 5:
+        for file_name in range(len(file_list) - 5):
+            os.remove(directory + file_list[file_name])
+
+
+def create_backup_file(path: str) -> str:
     """Create the backup of the file to be changed.
-    :param path: path to the file to changed"""
+    :param path: path to the file to changed
+    :return: path to the directory where the backup is"""
     directory = create_backup_folder()
-    backup_file_name = path.split('.')[0] + '_' + datetime.now().strftime('%Y%m%d%H%M')
+    backup_file_name = path.split('.')[0] + '_' + datetime.now().strftime('%Y%m%d%H%M%S')
     full_path = f'{directory}/{backup_file_name}'
     try:
         with open(path, 'r', encoding='utf-8') as file:
@@ -43,6 +54,8 @@ def create_backup_file(path: str):
     with open(full_path, 'w', encoding='utf-8') as file:
         file.write(file_contents)
 
+    return directory
+
 
 def search_and_replace(path: str = '', search: str = '', replace: str = ''):
     """Replace searched word in the file from the path.
@@ -50,7 +63,8 @@ def search_and_replace(path: str = '', search: str = '', replace: str = ''):
     :param search: word to be replaced
     :param replace: word to be used to replace the searched word"""
 
-    create_backup_file(path)
+    directory_for_backup = create_backup_file(path)
+    check_how_many_files(directory_for_backup)
 
     try:
         with open(path, 'r', encoding='utf-8') as file:
